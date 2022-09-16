bd6362e3f009ba50174d90711c0e4c21b7b23da0
shaza0q <90998258+shaza0q@users.noreply.github.com>
2022-09-17 01:34:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {Footer,Header,Blog,Features,Possibility,WhatGPT3} from './containers';
+import {Navbar,Brand,Cta} from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './features.css';
+
+
+const Feature = () => {
+  return (
+    <div>
+      Features
+    </div>
+  )
+}
+
+export default Feature
